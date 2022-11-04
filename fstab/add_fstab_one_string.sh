0acecde38d3f77880a5aae32ab52eb40b55c6982
Alexandr <alex.s.korzhov@gmail.com>
2022-11-04 11:07:58 +0600

    bugfix

--- fstab/add_fstab_one_string.sh
+++ fstab/add_fstab_one_string.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DEVICE="/dev/sda"; MOUNT_POINT=$(df | grep "${DEVICE}" | awk '{print $6}');FS_TYPE=$(df -T| grep "${DEVICE}" | awk '{print $2}') ; blkid -o export "${DEVICE}" | grep "UUID" | awk -v MOUNT_POINT="${MOUNT_POINT}" -v FS_TYPE="${FS_TYPE}" '{print $1 " " MOUNT_POINT " " FS_TYPE " defaults,noatime 0 2"}' >> /etc/fstab
+DEVICE="/dev/sda"; MOUNT_POINT=$(df | grep "${DEVICE}" | awk '{print $6}');FS_TYPE=$(df -T| grep "${DEVICE}" | awk '{print $2}') ; blkid -o export "${DEVICE}" | grep "^UUID" | awk -v MOUNT_POINT="${MOUNT_POINT}" -v FS_TYPE="${FS_TYPE}" '{print $1 " " MOUNT_POINT " " FS_TYPE " defaults,noatime 0 2"}' >> /etc/fstab
